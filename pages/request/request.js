a5f7ec27233dfb7b3f4d08984ff8070367de77ae
ds172827443 <172827443@qq.com>
2020-10-26 14:44:02 +0800

    网络请求

---
+++ pages/request/request.js
@@ -0,0 +1,78 @@
+// pages/request/request.js
+import request from '../../network/network'
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    request({
+      url:'http://152.136.185.210:8000/api/w6/recommend',
+      method:'get',
+      data:{
+        name:'dongshun',
+        age:18
+      }
+    }).then(res =>{
+      console.log(res);   
+    }).catch(err =>{
+      console.log(err);   
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
